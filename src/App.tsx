3ae45ca88be5af833c8f9eb6cc2896df6e48b3e3
AndrianinaHa <yankeenantsou@gmail.com>
2021-09-14 16:21:12 +0300

    another one

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,109 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface IFormInput {
+  email: string;
+  firstName: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  firstName: yup.string().required().min(2).max(25),
+  password: yup.string().required().min(8).max(120),
+});
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    textAlign: "center",
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 function App() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+  
+  const { heading, submitButton } = useStyles();
+
+  const [json, setJson] = useState<string>();
+
+  const onSubmit = (data: IFormInput) => {
+    setJson(JSON.stringify(data));
+  };
+
   return (
-    <div className="App">
-      
-
-
-
-
-
-
-
-      
-    </div>
+    <Container maxWidth="xs">
+      <Typography className={heading} variant="h3">
+        Sign Up Form
+      </Typography>
+      <form>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="Email"
+          fullWidth
+          required
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="First Name"
+          fullWidth
+          required
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          label="Password"
+          type="password"
+          fullWidth
+          required
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={submitButton}
+        >
+          Sign Up
+        </Button>
+        {json && (
+          <>
+            <Typography variant="body1">
+              Below is the JSON that would normally get passed to the server
+              when a form gets submitted
+            </Typography>
+            <Typography variant="body2">{json}</Typography>
+          </>
+        )}
+      </form>
+    </Container>
   );
 }
 
-export default App;
+export default App ;
